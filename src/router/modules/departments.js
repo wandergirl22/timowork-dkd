14d6e4d267c502af08e5240991d3cb3941863b10
zach1zou <791522322@qq.com>
2022-08-07 11:29:51 +0800

    对自己负责的点位管理模块进行修改

--- src/router/modules/departments.js
+++ src/router/modules/departments.js
@@ -1,18 +1,23 @@
 import Layout from '@/layout'
 export default {
-  path: '/departments',
+  path: '/node',
   component: Layout,
   meta: { title: '点位管理', icon: 'PT' },
   children: [
     {
-      path: '',
-      component: () => import('@/views/departments/components/business'),
-      meta: { title: '运营工单' }
+      path: '/node/region',
+      component: () => import('@/views/departments/components/region'),
+      meta: { title: '区位管理' }
     },
     {
-      path: '/departments/operation',
-      component: () => import('@/views/departments/components/operation'),
-      meta: { title: '运维工单' }
+      path: '/node/node',
+      component: () => import('@/views/departments/components/node'),
+      meta: { title: '点位管理' }
+    },
+    {
+      path: '/node/partner',
+      component: () => import('@/views/departments/components/partner'),
+      meta: { title: '合作商管理' }
     }
   ]
 }
---
+++ src/views/departments/components/partner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>合作商管理</div>
+</template>
+<script>
+export default {
+  props: {
+    arr: {
+      type: Array,
+      default: () => []
+    },
+    obj: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  computed: {},
+  watch: {},
+  updated() {},
+  beforeDestroy() {},
+  filters: {}
+}
+</script>
+<style lang="less" scoped></style>
