93c837fdb5546e358f1cd495818c1e55a58e8e41
SweetGirl <1762088644@qq.com>
2022-08-08 08:46:18 +0800

    8/7

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import Vue from "vue";
+import Vue from 'vue'
 
-import "normalize.css/normalize.css"; // A modern alternative to CSS resets
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-import locale from "element-ui/lib/locale/lang/en"; // lang i18n
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
-import "@/styles/index.scss"; // global css
+import '@/styles/index.scss' // global css
 
-import App from "./App";
-import store from "./store";
-import router from "./router";
+import App from './App'
+import store from './store'
+import router from './router'
 
-import "@/icons"; // icon
-import "@/permission"; // permission control
+import '@/icons' // icon
+import '@/permission' // permission control
 
 // 注册自定义指令
-import * as directives from "@/directives";
+import * as directives from '@/directives'
+import 'echarts'
+import Echarts from 'vue-echarts'
+import dayjs from 'dayjs'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -25,25 +28,26 @@ import * as directives from "@/directives";
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === "production") {
-  const { mockXHR } = require("../mock");
-  mockXHR();
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
 }
-
+dayjs.locale('zh-cn')
+Vue.component('v-chart', Echarts)
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale });
+Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 // 注册自定义指令
 for (let key in directives) {
-  Vue.directive(key, directives[key]);
+  Vue.directive(key, directives[key])
 }
 
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
   store,
-  render: (h) => h(App),
-});
+  render: (h) => h(App)
+})
